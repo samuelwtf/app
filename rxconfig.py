e80e2eff1a18b54307dca6ad1c616abd199156c9
samuelwtf <72716498+samuelwtf@users.noreply.github.com>
2025-06-24 16:23:56 -0500

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="system.app",  # ¡CRÍTICO! Esto le dice a Reflex dónde está tu módulo principal
-    api_url="http://0.0.0.0:8000", # URL donde tu backend de Reflex estará escuchando
-    # Puedes añadir otras configuraciones aquí si las necesitas
+    app_name="app"
 )
