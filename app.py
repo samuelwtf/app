262b42987bbdd9b74a1f14e1ff53e725e222a946
samuelwtf <72716498+samuelwtf@users.noreply.github.com>
2025-06-24 16:22:54 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import reflex as rx
+
+class State(rx.State):
+    count: int = 0
+
+    def increment(self):
+        self.count += 1
+
+def index():
+    return rx.vstack(
+        rx.heading("Hola desde Reflex 🚀"),
+        rx.text(f"Contador: {State.count}"),
+        rx.button("Incrementar", on_click=State.increment)
+    )
+
+app = rx.App()
+app.add_page(index)
