5e051659f1ade26277a71111a5a7dd3d3d042978
samuelwtf <72716498+samuelwtf@users.noreply.github.com>
2025-06-26 15:22:07 -0500

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,11 @@ import reflex as rx
 
 config = rx.Config(
     app_name="app",
+    frontend_port=3000,
+    backend_port=8000,
+    # Configuraciones adicionales para producción
+    env=rx.Env.PROD,
+    # Si usas base de datos
+    # db_url="postgresql://user:pass@host:port/dbname"
     plugins=[rx.plugins.TailwindV3Plugin()]
 )
