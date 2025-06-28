333078d82a6196b78afbd2ae59203bf3acd86eaf
samuelwtf <72716498+samuelwtf@users.noreply.github.com>
2025-06-28 10:34:21 -0500

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="app",
+    show_built_with_reflex=False,
     frontend_port=3000,
     backend_port=8000,
     # Configuraciones adicionales para producción
